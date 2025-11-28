b61e17be532580cf23960018477bacefe5b80ce5
valenhung2502 <valenhung2502@gmail.com>
2025-11-27 22:35:06 -0300

    cambio 3

--- borrador_controlador_app.py
+++ borrador_controlador_app.py
@@ -13,14 +13,23 @@ class Controlador:
             raise Exception(f"Error al obtener los viajes: {str(e)}")
 
     def agregar_viaje(self, dni, nombre, apellido, pasaje, horario, destino, fecha):
+
+        if not self.modelo.validar_dni(dni):
+            raise Exception("El DNI no es válido")
+
+        if not self.modelo.validar_nombre(nombre):
+            raise Exception("El nombre no es válido")
+
         return self.modelo.agregar(dni, nombre, apellido, pasaje, horario, destino, fecha)
 
     def borrar_viaje(self, dni):
+
+        if not self.modelo.validar_dni(dni):
+            raise Exception("DNI inválido")
+
         return self.modelo.borrar(dni)
 
 if __name__ == '__main__':
-
     from intento_vista_app_viajes import App_viajes
-
     app = App_viajes()
     app.mainloop()
